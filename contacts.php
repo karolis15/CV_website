94e472866af4f8f0b8df4e79c3f4eca2ca8ada4f
karolis15 <karolisgied@gmail.com>
2022-05-19 11:28:29 +0300

    Add files via upload

---
+++ contacts.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <!-- nav -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-grey">
+  <a class="navbar-brand text-success" id = "logo" href="index.php">Karolis</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="work.php">My Work<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="contacts.php">Contacts <span class="sr-only">(current)</span></a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
+  <!-- nav -->
+  <!-- hero -->
+<section id="hero">
+<div class="hero container">
+<div>
+  <h1>Contact me<span></span></h1>
+
+
+</div>
+</div>
+</section>
+ <!-- hero -->
+<!--  contacts -->
+
+<!--  contacts -->
+
+ 
+
+
+</body>
+</html>
---
+++ work.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <!-- nav -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-grey">
+  <a class="navbar-brand text-success" id = "logo" href="index.php">Karolis</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="work.php">My Work<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="contacts.php">Contacts <span class="sr-only">(current)</span></a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
+  <!-- nav -->
+  <!-- hero -->
+<section id="hero">
+<div class="hero container">
+<div>
+  <h1>My Work<span></span></h1>
+
+  <!-- <a href="#" type="button" class="cta">Contacts</a> -->
+</div>
+</div>
+</section>
+ <!-- hero -->
+ <!-- my work -->
+
+<div class="card-deck">
+
+  <div class="card" >
+      <a href="index.php">
+    <img class="card-img-top" src="img/card1.png" alt="Card image cap" >
+    </a>
+    <div class="card-body" >
+      <h5 class="card-title" href="index.php" >Card title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+       
+  <div class="card" >
+      <a href="index.php">
+    <img class="card-img-top" src="img/card1.png" alt="Card image cap" >
+    </a>
+    <div class="card-body" >
+      <h5 class="card-title" href="index.php" >Card title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+
+  <div class="card" >
+      <a href="index.php">
+    <img class="card-img-top" src="img/card1.png" alt="Card image cap" >
+    </a>
+    <div class="card-body" >
+      <h5 class="card-title" href="index.php" >Card title</h5>
+      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+
+
+ <!-- my work -->
+
+</body>
+</html>
